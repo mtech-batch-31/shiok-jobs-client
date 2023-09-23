768b0a21ef2e3e44dade016c8d3b8c51e5d1d807
Wang Shiji <scglwsj@163.com>
2023-09-23 15:29:40 +0800

    change the login page

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,16 +1,15 @@
 // import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 // import { IoIosLogOut } from "react-icons/io"
 import { ACCESS_TOKEN, ID_TOKEN, REFRESH_TOKEN } from "../utilities/constants";
 import { NavLink, useNavigate } from "react-router-dom";
 import React, { useEffect } from "react";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 // import logo from '../Assets/word-logo.png';
 import axios, { AxiosResponse } from "axios";
-import './Header.css'
-
+import "./Header.css";
 
 interface AuthResponse {
   id_token: string;
@@ -28,112 +27,131 @@ interface AuthRequest {
   redirect_uri: string;
 }
 
-
 function Header() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    // let hasTokenValue = Cookies.get(ACCESS_TOKEN) !== undefined;
-    //console.log(hasTokenValue);
-    // const navigate = useNavigate();
+  // let hasTokenValue = Cookies.get(ACCESS_TOKEN) !== undefined;
+  //console.log(hasTokenValue);
+  // const navigate = useNavigate();
 
-    // const logout = () =>
-    // {
-    //     Cookies.remove(ACCESS_TOKEN);
-    //     navigate('/');
-    // }
-    useEffect(() => {
-      const queryParameters = new URLSearchParams(window.location.search);
-      const authCode = queryParameters.get("code") as string;
-      // console.log(`authorization code=${authCode}`);
-  
-      let request: AuthRequest = {
-        code: authCode,
-        grant_type: "authorization_code",
-        client_id: process.env.REACT_APP_COGNITO_CLIENT_ID as string,
-        client_secret: process.env.REACT_APP_COGNITO_CLIENT_SECRET as string,
-        redirect_uri: process.env.REACT_APP_COGNITO_LOGIN_REDIRECT_URL as string
-      };
-  
-      // const token = Cookies.get(ACCESS_TOKEN);
-      // TO DO: discuss where to verify token
-      if (authCode) {
-        getAuthToken();
+  // const logout = () =>
+  // {
+  //     Cookies.remove(ACCESS_TOKEN);
+  //     navigate('/');
+  // }
+  useEffect(() => {
+    const queryParameters = new URLSearchParams(window.location.search);
+    const authCode = queryParameters.get("code") as string;
+    // console.log(`authorization code=${authCode}`);
+
+    let request: AuthRequest = {
+      code: authCode,
+      grant_type: "authorization_code",
+      client_id: process.env.REACT_APP_COGNITO_CLIENT_ID as string,
+      client_secret: process.env.REACT_APP_COGNITO_CLIENT_SECRET as string,
+      redirect_uri: process.env.REACT_APP_COGNITO_LOGIN_REDIRECT_URL as string,
+    };
+
+    // const token = Cookies.get(ACCESS_TOKEN);
+    // TO DO: discuss where to verify token
+    if (authCode) {
+      getAuthToken();
+    }
+
+    async function getAuthToken() {
+      try {
+        const response: AxiosResponse = await axios.post(
+          process.env.REACT_APP_COGNITO_AUTH_TOKEN_URL as string,
+          request,
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        );
+        console.log("successfully retrieved token");
+        navigate("", { replace: true }); // This will navigate to the same URL without query parameters
+        const authResponse = response?.data as AuthResponse;
+
+        // console.log("auth response", authResponse);
+        Cookies.set(ACCESS_TOKEN, authResponse.access_token, { path: "/" });
+        Cookies.set(ID_TOKEN, authResponse.id_token, { path: "/" });
+        Cookies.set(REFRESH_TOKEN, authResponse.refresh_token, { path: "/" });
+      } catch (error) {
+        console.log("error in getting auth token: ", error);
+        Cookies.remove(ACCESS_TOKEN);
+        Cookies.remove(ID_TOKEN);
+        Cookies.remove(REFRESH_TOKEN);
       }
-  
-      async function getAuthToken() {
-        try {
-          const response: AxiosResponse = await axios.post(
-            process.env.REACT_APP_COGNITO_AUTH_TOKEN_URL as string,
-            request,
-            {
-              headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-              },
-            }
-          );
-          console.log("successfully retrieved token");
-          navigate('', { replace: true }); // This will navigate to the same URL without query parameters
-          const authResponse = response?.data as AuthResponse;
-          
-          // console.log("auth response", authResponse);
-          Cookies.set(ACCESS_TOKEN, authResponse.access_token, { path: "/" });
-          Cookies.set(ID_TOKEN, authResponse.id_token, { path: "/" });
-          Cookies.set(REFRESH_TOKEN, authResponse.refresh_token, { path: "/" });
-        } catch (error) {
-          console.log("error in getting auth token: ", error);
-          Cookies.remove(ACCESS_TOKEN);
-          Cookies.remove(ID_TOKEN);
-          Cookies.remove(REFRESH_TOKEN);
-        }
-      }
-    }, [navigate]);
+    }
+  }, [navigate]);
 
-
-
-  let loginBaseUrl = "https://shiokjobs.auth.ap-southeast-1.amazoncognito.com/login?client_id=1vae5aaf8bra5o00lqi56doioq&response_type=code&scope=email+openid+phone&redirect_uri=";
-  let loginRedirectUrl = process.env.REACT_APP_COGNITO_LOGIN_REDIRECT_URL as string; 
-  let loginUrl = loginBaseUrl + loginRedirectUrl;
   return (
     <Navbar expand="md" className="bg-body-tertiary px-4">
       {/* <Container> */}
-        <Navbar.Brand className="" href="/">
-            {/* <img className="nav-logo" src={logo} alt="logo"></img> */}
-            <div className="nav-logo fw-bold">shiok jobs</div>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Brand className="" href="/">
+        {/* <img className="nav-logo" src={logo} alt="logo"></img> */}
+        <div className="nav-logo fw-bold">shiok jobs</div>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <Navbar.Collapse id="basic-navbar-nav custom-nav">
-          <Nav className="ms-auto  mx-3">
-            {/* <Nav.Link className="ms-4"> */}
-            <div className='nav-link ms-4'>
-
-            
-                    <NavLink  className={({isActive}) => isActive? 'nav-item-active':'nav-item'} to="/job">Search Job</NavLink>
-                {/* </Nav.Link> */}
-            </div>
-            {/* <Nav.Link className="ms-4"> */}
-            <div className='nav-link ms-4'>
-                <NavLink  className={({isActive}) => isActive? 'nav-item-active':'nav-item'} to="/">Company Reviews</NavLink>
-            </div>
-            {/* </Nav.Link> */}
-            {/* <Nav.Link className="ms-4"> */}
-            <div className='nav-link ms-4'>
-                <NavLink  className={({isActive}) => isActive? 'nav-item-active ':'nav-item '} to="/">Career Guide</NavLink>
-            </div>
-            {/* </Nav.Link> */}
-            {/* <Nav.Link className="ms-4 "> */}
-            <div className='nav-link ms-4'>
-                <NavLink  className={({isActive}) => isActive? 'nav-item-active gold' :'nav-item gold'} to="/">Employer</NavLink>
-            </div>
-            {/* </Nav.Link> */}
-            <Nav.Link className="ms-4 nav-item blue fw-bold" 
-          
-            href={loginUrl}
+      <Navbar.Collapse id="basic-navbar-nav custom-nav">
+        <Nav className="ms-auto  mx-3">
+          {/* <Nav.Link className="ms-4"> */}
+          <div className="nav-link ms-4">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "nav-item-active" : "nav-item"
+              }
+              to="/job"
             >
+              Search Job
+            </NavLink>
+            {/* </Nav.Link> */}
+          </div>
+          {/* <Nav.Link className="ms-4"> */}
+          <div className="nav-link ms-4">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "nav-item-active" : "nav-item"
+              }
+              to="/"
+            >
+              Company Reviews
+            </NavLink>
+          </div>
+          {/* </Nav.Link> */}
+          {/* <Nav.Link className="ms-4"> */}
+          <div className="nav-link ms-4">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "nav-item-active " : "nav-item "
+              }
+              to="/"
+            >
+              Career Guide
+            </NavLink>
+          </div>
+          {/* </Nav.Link> */}
+          {/* <Nav.Link className="ms-4 "> */}
+          <div className="nav-link ms-4">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "nav-item-active gold" : "nav-item gold"
+              }
+              to="/"
+            >
+              Employer
+            </NavLink>
+          </div>
+          {/* </Nav.Link> */}
+          <div className="nav-link ms-4">
+            <NavLink className="nav-item blue fw-bold" to="/login">
               Login
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+            </NavLink>
+          </div>
+        </Nav>
+      </Navbar.Collapse>
       {/* </Container> */}
     </Navbar>
   );
