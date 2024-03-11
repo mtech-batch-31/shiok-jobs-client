2b4598fc3886a75246f406dfa3413397008fd476
ernestjx <73307945+ernestjx@users.noreply.github.com>
2024-03-11 23:43:35 +0800

    build.yml

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,13 +1,12 @@
 export const API_URL = {
-    LOGIN : `${process.env.REACT_APP_SHIOK_JOBS_BFF_URL}/auth/login`,
-    LOGOUT : `${process.env.REACT_APP_SHIOK_JOBS_BFF_URL}/auth/logout`,
-    REGISTER: `${process.env.REACT_APP_SHIOK_JOBS_BFF_URL}/auth/signup`,
-    REFRESH_TOKEN: `${process.env.REACT_APP_SHIOK_JOBS_BFF_URL}/auth/refresh`,
-    REG_CONFIRM: `${process.env.REACT_APP_SHIOK_JOBS_BFF_URL}/auth/ConfirmSignup`,
-    USER_PROFILE: `${process.env.REACT_APP_SHIOK_JOBS_BFF_URL}/api/user/v1/user/me`,
-    UPDATE_PROFILE: `${process.env.REACT_APP_SHIOK_JOBS_BFF_URL}/api/user/v1/user`,
-    JOBS: `${process.env.REACT_APP_SHIOK_JOBS_BFF_URL}/api/job/v1/jobs`,
-    JOBS_AUTH: `${process.env.REACT_APP_SHIOK_JOBS_BFF_URL}/api/job/v1/jobs/auth`
+    LOGIN : `${process.env.REACT_APP_GATEWAY_URL}/shiokjobs/auth/login`,
+    REGISTER: `${process.env.REACT_APP_GATEWAY_URL}/shiokjobs/auth/signup`,
+    REFRESH_TOKEN: `${process.env.REACT_APP_GATEWAY_URL}/shiokjobs/auth/refresh`,
+    REG_CONFIRM: `${process.env.REACT_APP_GATEWAY_URL}/shiokjobs/v1/user/registrationConfirm`,
+    USER_PROFILE: `${process.env.REACT_APP_GATEWAY_URL}/shiokjobs/user/v1/user/me`,
+    UPDATE_PROFILE: `${process.env.REACT_APP_GATEWAY_URL}/shiokjobs/user/v1/user`,
+    JOBS: `${process.env.REACT_APP_GATEWAY_URL}/shiokjobs/job/v1/jobs`,
+    JOBS_AUTH: `${process.env.REACT_APP_GATEWAY_URL}/shiokjobs/job/v1/jobs/auth`
 }
 
 export const AUTH = {
