72cda03a86a2a1a2e711d230218848cae1654c3a
weileong123 <ilovehci@users.noreply.github.com>
2024-04-04 23:20:20 +0800

    add logging

--- src/Page/UserProfile.tsx
+++ src/Page/UserProfile.tsx
@@ -16,6 +16,7 @@ const Home: React.FC = () => {
 
     let url = API_URL.USER_PROFILE
     console.log(`calling ${url}`);
+    console.log('add loggng');
     axiosInstance
       .get(
         url
